e17816fc3eed6d01a518947f3936b774aa13bcd2
yotam-biu <nagaryo3@biu.ac.il>
2024-11-23 19:47:08 +0200

    Update string_utils.py

--- string_utils.py
+++ string_utils.py
@@ -1,6 +1,25 @@
 def split_before_each_uppercases(formula):
-    pass # Replace the pass with your code
+    if len(formula) == 0:
+       return []
+    start = 0
+    end = 1
+    split_formula = []
+    for char in formula[1:]:
+        if char.isupper():
+            split_formula.append(formula[start:end])
+            start = end
+        end += 1
+    split_formula.append(formula[start:end])
+    return split_formula
 
 
 def split_at_first_digit(formula):
-    pass # Replace the pass with your code
+    digit_location = 1
+    for char in formula[1:]:
+        if char.isdigit():
+            break
+        digit_location += 1
+    if digit_location == len(formula):
+        return formula, 1
+    else:
+        return formula[:digit_location], int(formula[digit_location:])
